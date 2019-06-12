2dc30dd7c914ec41407b49ecb8c0cc1e813a5a59
Tom White <eugene.white@gmail.com>
2019-06-12 04:00:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    
+  end
+  
+  
 end
